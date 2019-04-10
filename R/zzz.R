f188492b30b03b5bc9336eddfdf69a37635fea09
ekothe <pc.miss@gmail.com>
2019-04-10 13:33:21 +1000

    Total rebuild based on tidyverse style structure

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+.onAttach <- function(...) {
+  needed <- core[!is_attached(core)]
+  if (length(needed) == 0)
+    return()
+
+  crayon::num_colors(TRUE)
+  metaverse_attach()
+
+
+}
+
+is_attached <- function(x) {
+  paste0("package:", x) %in% search()
+}
