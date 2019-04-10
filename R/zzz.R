5d8a7e94ccc97ad7b9eb3c00b1cfcc5e271afade
cantabile <charlestigray@gmail.com>
2019-04-10 10:06:32 +1000

    added data export :notes:

--- R/zzz.R
+++ R/zzz.R
@@ -14,14 +14,21 @@ NULL
 
 #' @export
 revtools::screen_titles
+
 #' @export
 revtools::screen_abstracts
+
 #' @export
 revtools::screen_topics
+
 #' @export
 revtools::screen_duplicates
 
 #' @export
 metafor::rma
+
 #' @export
 metafor::escalc
+
+#' @export
+metafor::dat.bcg
